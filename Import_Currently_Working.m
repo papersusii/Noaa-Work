66bde9ac9c13fb8656e77a70476e09280903b419
Ian A <ian.adler.nc@gmail.com>
2016-06-15 15:34:53 -0400

    Added a LOT. Fixed the odd textData files not working, redid the 2 minute averaging system. also added an extremely easy (and probably incorrect) method of ignoring the even/odds using a try and catch thing. It;ll probably work though

--- Import_Currently_Working.m
+++ Import_Currently_Working.m
@@ -20,6 +20,7 @@ just run the command that issues a warning, then directly after run
 tableData=readtable('BAO_OZ3_2014001.dat'); %Don't use this if you have a different error
 [a, MSGID] = lastwarn();
 warning('off', MSGID)
+clear tableData a MSGID
 
 You can then delete MSGID and a. Be warned, I don't know how
 to turn the warning back on (although it's proably pretty simple) so
@@ -33,7 +34,7 @@ clear all
 clc
 tic
 startNum = 1;
-endNum = 1;
+endNum = 500;
 
 
 startNumForCalc=startNum;
@@ -56,7 +57,7 @@ for startNum = startNum:endNum
 		if numel(num2str(startNum)) == 3;
 			titleText = ['BAO_OZ3_2014' num2str(startNum) '.dat'];
 		end
-		cd C:\Users\Ian\Documents\MATLAB\DATA_RAW
+		cd C:\Users\Ian\Documents\MATLAB\DATA_RAW %potential error point
 		if exist(titleText,'file') == 2
 			
 			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Imports Data
@@ -74,12 +75,9 @@ for startNum = startNum:endNum
 					findNan=char(numData.numData(x,1));
 					
 					if ((findNan(1) ~= 'N' ) && (findNan(1) ~= '-')) == 1
-						%numDataClean(x,1)=str2num(char(numData.numData(x,1)));
 						numDataClean(x,1)=str2double(char(numData.numData(x,1)));
 					else
 						numDataClean(x,1)=NaN;
-						%warning(['FILE ' textIncrString ' at ' num2str(x)]);
-						%warning(['invalid @ ' num2str(x) ' (+1 for .dat)']);
 					end
 				end
 			else
@@ -100,7 +98,7 @@ for startNum = startNum:endNum
 			numDataClean(numDataClean<.001)=NaN;
 			
 			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Average for 2 min
-			
+			%{
 			%disp(['FILE ' num2str(startNum) '.dat SUCCESSFUL'])
 			clear numDataOdd numData Even numDataHold x
 			numDataOdd=numDataClean(1:2:end,1);
@@ -130,69 +128,151 @@ for startNum = startNum:endNum
 					numDAvgOneMin(x)=(sum(numDataHold(~isnan(numDataHold)))/2);
 				end
 			end
-			
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Average for 5 min
+			%}
 			try
 				for count=1:length(numDataClean)
 					if count==1
-						numDataHold=numDataClean(1:9,1);
-						numDAvgFiveMin(1,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(1:9,1))))));
+						numDataHold=numDataClean(1:2,1);
+						numDAvgOneMinNew(1,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(1:2,1))))));
 					else
-						numDataHold=numDataClean(((count-1)*10):((count-1)*10+9),1);
-						numDAvgFiveMin(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*10):((count-1)*10+9),1))))));
+						numDataHold=numDataClean(((count-1)*2+1):((count)*2),1);
+						numDAvgOneMinNew(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*2+1):((count)*2),1))))));
 						
 					end
 				end
 			catch
-				disp('too muich rim make the ride too hard');
+				numDataHold=numDataClean(((count-1)*2+1):end,1);
+				numDAvgOneMinNew(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*2+1):end,1))))));
+				
+				%disp('2 minute average failed');
+				
+			end
+			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Average for 5 min
+			try
+				for count=1:length(numDataClean)
+					if count==1
+						numDataHold=numDataClean(1:10,1);
+						numDAvgFiveMin(1,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(1:10,1))))));
+					else
+						numDataHold=numDataClean(((count-1)*10+1):((count)*10),1);
+						numDAvgFiveMin(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*10+1):((count)*10),1))))));
+						
+					end
+				end
+			catch
+				numDataHold=numDataClean(((count-1)*10+1):end,1);
+				
+				%disp('5 min average SUCCESSFUL');
+				numDAvgFiveMin(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*10+1):end,1))))));; %how does this work? idk but it doees
 			end
 			
 			%%%%%%%%%%%%%convert and combine 2 min, easily changed for 5/60
-			
+			clear count
 			%oneMinFullSet=cell(length(numDAvgOneMin),1);
 			textDataEven=textData(2:2:end,1);
 			cd C:\Users\Ian\Documents\MATLAB
-			for count=1:length(numDAvgOneMin+1)
-				if count==1;
-					fid=fopen(['MyFile_'  num2str(startNum) '.txt'],'w');
-					header = 'STN YEAR  MON  DAY  HR  MIN  O3(PPB)';
-					fprintf(fid, [ header '\n']);
-					
-				else
-					numDataText=num2str(numDAvgOneMin(count-1));
-					textDatLine=char(textDataEven(count-1));
-					%numDataText=num2str(numDataText);
-					if numel(numDataText) == 8 %100
-						numDataText=numDataText(1:6);
-						
-						textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0   ' numDataText]);
-						fprintf(fid, [ textLine '\n']);
-						
-					elseif numel(numDataText) == 7 %10
-						numDataText=numDataText(1:5);
-						textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0    ' numDataText]);
-						fprintf(fid, [ textLine '\n']);
-						
-					elseif 	numel(numDataText) == 6 %1
-						numDataText=numDataText(1:4);
-						textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0     ' numDataText]);
-						fprintf(fid, [ textLine '\n']);
-						
+			try
+				for count=1:length(numDAvgOneMinNew+1)
+					if count==1;
+						fid=fopen(['OneMin_'  num2str(startNum) '.txt'],'w');
+						header = 'STN YEAR  MON  DAY  HR  MIN  O3(PPB)';
+						fprintf(fid, [ header '\n']);
 					else
-						warning(['Inputing NaN for line ' num2str(count)]);
-						textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0      NaN']);
-						fprintf(fid, [ textLine '\n']);
+						numDataText=num2str(numDAvgOneMinNew(count-1));
+						textDatLine=char(textDataEven(count-1));
+						%numDataText=num2str(numDataText);
+% 						disp(count)
+						if numel(numDataText) == 8 %100
+							numDataText=numDataText(1:6);
+							
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0   ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+							
+						elseif numel(numDataText) == 7 %10
+							numDataText=numDataText(1:5);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0    ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+						elseif 	numel(numDataText) == 6 %1
+							numDataText=numDataText(1:4);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0     ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+						else
+							%warning(['Inputing NaN for line ' num2str(count)]);
+							%%%%%%THE ABOVE LINE CAN BE UNCOMMENTED TO
+							%%%%%%CHECK WHICH SPECIFIC LINES HAVE BEEN
+							%%%%%%COMMENTED!!!
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0      NaN']);
+							fprintf(fid, [ textLine '\n']);
+						end
+						
 					end
-					
 				end
+			catch
+				disp(['2 min average failed on file ' num2str(startNum)])
 			end
-			disp(['MyFile_'  num2str(startNum) '.txt SUCCESSFUL']);
+			disp(['OneMin_'  num2str(startNum) '.txt SUCCESSFUL']);
 			fclose(fid);
 			
 			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+			textDataFive=textData(1:10:end,1);
+			for count=1:length(textDataFive)
+				holdout=char(textDataFive(count,1));
+				holdout=holdout(1:17);
+				holdout=[holdout '00'];
+				textDataFive(count,1)=cellstr(holdout);
+			end
 			
+			cd C:\Users\Ian\Documents\MATLAB
+			try
+				for count=1:(length(numDAvgFiveMin)+1)
+					if count==1;
+						fid=fopen(['FiveMin_'  num2str(startNum) '.txt'],'w');
+						header = 'STN YEAR  MON  DAY  HR  MIN  O3(PPB)';
+						fprintf(fid, [ header '\n']);
+						
+					else
+						numDataText=num2str(numDAvgFiveMin(count-1));
+						textDatLine=char(textDataFive(count-1));
+						%numDataText=num2str(numDataText);
+						if numel(numDataText) == 8 %100
+							numDataText=numDataText(1:6);
+							
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0   ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+						elseif numel(numDataText) == 7 %10
+							numDataText=numDataText(1:5);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0    ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+						elseif 	numel(numDataText) == 6 %1
+							numDataText=numDataText(1:4);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0     ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+							
+						else
+							warning(['Inputing NaN for line ' num2str(count)]);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0      NaN']);
+							fprintf(fid, [ textLine '\n']);
+						end
+						
+					end
+				end
+			catch
+				disp(['5 min average failed on file ' num2str(startNum)])
+			end
+			disp(['FiveMin_'  num2str(startNum) '.txt SUCCESSFUL']);
+			fclose(fid);
+			
+			
+			
+			
+			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		else
-			warning([ titleText  ' DOES NOT EXIST'])
+			%warning([ titleText  ' DOES NOT EXIST'])
 			%fileIsReal=0;
 		end
 	else
@@ -205,14 +285,14 @@ time=toc;
 disp('----------------------------');
 
 if time < 60
-disp([ num2str(time) ' seconds elapsed']);
+	disp([ num2str(time) ' seconds elapsed']);
 else
-disp([num2str(time/60) ' minutes elapsed']);
+	disp([num2str(time/60) ' minutes elapsed']);
 end
 
 if endNum-startNumForCalc ~= 0
 	disp([ num2str((abs((time/(endNum-startNumForCalc))))) ' seconds/file']);
-
+	
 else
 	disp([ num2str(time) ' seconds/file']);
 end
@@ -223,7 +303,7 @@ fclose all;
 clear structData
 
 clear tableData  numDataOdd numDataHold numDataEven numDataClean
-clear avgSet count endNum fileIsReal header incr3 oneMinDateLine oneMinNumLine 
+clear avgSet count endNum fileIsReal header incr3 oneMinDateLine oneMinNumLine
 clear startnum textIncrString textLine titleText
 clear startNum y.qyBgmx.pDrne
 clear time startNumForCalc numData textNumberHold x findNan
@@ -274,7 +354,7 @@ S(l,1) = 0;
 time = datetime(Y,M,D,H,MI,S);
 data = myImport.data(:,7);
 data(data>65)=NaN;
-plot(time,data);         
+plot(time,data);
 ylabel('Ozone ppb');
 hold on;
 % x = x + 1;
@@ -296,7 +376,7 @@ S(l,1) = 0;
 time = datetime(Y,M,D,H,MI,S);
 data = myImport.data(:,7);
 data(data>65)=NaN;
-plot(time,data,'r');         
+plot(time,data,'r');
 ylabel('Ozone ppb');
 hold on;
 % x = x + 1;
