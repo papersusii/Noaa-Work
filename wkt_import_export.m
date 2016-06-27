1346d065f06bec6711e3434a27f4e778e409a0b3
Ian A <ian.adler.nc@gmail.com>
2016-06-27 13:26:27 -0400

    asdf

--- wkt_import_export.m
+++ wkt_import_export.m
@@ -12,18 +12,21 @@ hourText={'00' '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12' '13' 
 
 %direxportdata
 
-for yearNum=1:8
-	for monthNum=1:12
+for yearNum=1:1
+	for monthNum=5:5
 		cd(['C:\Users\Ian\Documents\MATLAB\' char(yearText(yearNum)) '\' char(monthText(monthNum))])
 		disp(['C:\Users\Ian\Documents\MATLAB\' char(yearText(yearNum)) '\' char(monthText(monthNum))])
 		for dayNum = 1:31
+					cd(['C:\Users\Ian\Documents\MATLAB\' char(yearText(yearNum)) '\' char(monthText(monthNum))])
+		disp(['C:\Users\Ian\Documents\MATLAB\' char(yearText(yearNum)) '\' char(monthText(monthNum))])
+
 			disp(num2str(dayNum));
 			for hourNum=1:24
 				try
 					rawImport=importdata(['wkt_ozo1_' char(yearText(yearNum)) '_' char(monthText(monthNum)) '_' char(dayText(dayNum)) '_' char(hourText(hourNum)) '00.dat']);
 					if hourNum==1
-					rawData=rawImport.data(:,2);
-					rawText=rawImport.textdata(3:end,1);
+						rawData=rawImport.data(:,2);
+						rawText=rawImport.textdata(3:end,1);
 					else
 						rawData=[rawData;rawImport.data(:,2)];
 						rawText=[rawText;rawImport.textdata(3:end,1)];
@@ -32,6 +35,68 @@ for yearNum=1:8
 					warning(['COULD NOT IMPORT wkt_ozo1_' char(yearText(yearNum)) '_' char(monthText(monthNum)) '_' char(dayText(dayNum)) '_' char(hourText(hourNum)) '00.dat']);
 				end
 			end
+			cd(['C:\Users\Ian\Documents\MATLAB\WKT\TextFiles_' char(yearText(yearNum))]);
+			%%%%%%%%%%
+			numDataClean=rawData;
+			numDataClean=numDataClean+.0001;
+			numDataClean(numDataClean>200)=NaN;
+			numDataClean(numDataClean<=5)=NaN;
+
+			try
+				for count=1:length(numDataClean)
+					if count==1
+						numDataHold=numDataClean(1:2,1);
+						numDAvgOneMinNew(1,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(1:2,1))))));
+					else
+						numDataHold=numDataClean(((count-1)*2+1):((count)*2),1);
+						numDAvgOneMinNew(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*2+1):((count)*2),1))))));
+					end
+				end
+			catch
+				numDataHold=numDataClean(((count-1)*2+1):end,1);
+				numDAvgOneMinNew(count,1)=(sum(numDataHold(~isnan(numDataHold)))/(sum(not(isnan(numDataClean(((count-1)*2+1):end,1))))));
+			end
+			%%%%%%%%%%%%
+			cd(['C:\Users\Ian\Documents\MATLAB\WKT\TextFiles_' char(yearText(yearNum))]);
+			
+			textDataEven=rawText(2:2:end,1);
+			numDAvgOneMinNew=numDAvgOneMinNew+.0001;
+			try
+				for count=1:(length(numDAvgOneMinNew)+1)
+					if count==1;
+						fid=fopen(['OneMin_' char(yearText(yearNum)) '_'  num2str(dayNum) '.txt'],'w');
+						header = 'STN YEAR  MON  DAY  HR  MIN  O3(PPB)';
+						fprintf(fid, [ header '\n']);
+					else
+						numDataText=num2str(numDAvgOneMinNew(count-1));
+						textDatLine=char(textDataEven(count-1));
+						if numel(numDataText) == 8 %100
+							numDataText=numDataText(1:6);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0   ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+						elseif numel(numDataText) == 7 %10
+							numDataText=numDataText(1:5);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0    ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+						elseif 	numel(numDataText) == 6 %1
+							numDataText=numDataText(1:4);
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0     ' numDataText]);
+							fprintf(fid, [ textLine '\n']);
+						else
+							%warning(['Inputing NaN for line ' num2str(count)]); %this line is unnecissary but will make the program look cooler
+							textLine=(['350 ' textDatLine(1:4) '   ' textDatLine(6:7) '  ' textDatLine(9:10) '  ' textDatLine(12:13) '  ' textDatLine(15:16) '.0      NaN']);
+							fprintf(fid, [ textLine '\n']);
+						end
+					end
+				end
+			catch
+				disp('something went worng')
+			end
+			disp(['OneMin_'  num2str(dayNum) '.txt SUCCESSFUL']);
+			fclose all;
+			%%%%%%%%%%%
+			
+			
 		end
 	end
 end
